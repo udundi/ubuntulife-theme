96c83dcbcb4ee042e61a3a655c5d9a38692f4ba2
Isaac Folmsbee <if@sent.com>
2022-10-05 15:21:27 -0400

    Search box prompt

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,8 @@ module.exports = {
     extend: {
       animation: {
         "slide-in-h": "slide-in-h 0.8s cubic-bezier(0.22, 1, 0.36, 1)",
+        "slide-in-v": "slide-in-v 0.5s cubic-bezier(0.22, 1, 0.36, 1)",
+        "fade-in": "fade-in 0.5s cubic-bezier(0.22, 1, 0.36, 1)",
       },
       keyframes: {
         "slide-in-h": {
@@ -24,6 +26,24 @@ module.exports = {
           "100%": {
             opacity: 1,
             left: "0px",
+          },
+        },
+        "slide-in-v": {
+          "0%": {
+            opacity: 0,
+            top: "-15%",
+          },
+          "100%": {
+            opacity: 1,
+            top: "0",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
           },
         },
       },
